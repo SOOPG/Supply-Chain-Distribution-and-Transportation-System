8c74ed139c3b2f43ab59089ac46c44193f50f046
SOOPG <wilem.mhsoo@gmail.com>
2023-04-16 09:12:16 +0800

    V0.35 Added Retailer to Retailer Distance
    -Added retailer-to-retailer distance

--- headers/generateRetailerSites.h
+++ headers/generateRetailerSites.h
@@ -15,7 +15,10 @@ class RetailerSites{
 private:
     std::string locations[10];
     double goodsWeight[10];
-    double distancesFromHQ[10];
+    int distancesFromHQ[10];
+    int retailerConnectedToDistance[10];
+    string retailerConnectedTo[10];
+
 
 public:
     RetailerSites(){
@@ -41,6 +44,30 @@ public:
         distancesFromHQ[8] = 40;
         distancesFromHQ[9] = 20;
 
+        // Retailers Connected To Retailer[] by skipping other retailer distance
+        retailerConnectedTo[0] = "C";//15km;
+        retailerConnectedTo[1] = "E";//25km;
+        retailerConnectedTo[2] = "E";//15km;
+        retailerConnectedTo[3] = "F";//30km;
+        retailerConnectedTo[4] = "H";//45km;
+        retailerConnectedTo[5] = "H";//20km;
+        retailerConnectedTo[6] = "I";//15km;
+        retailerConnectedTo[7] = "J";//30km;
+        retailerConnectedTo[8] = "G";//15km;
+        retailerConnectedTo[9] = "H";//30km;
+
+        retailerConnectedToDistance[0] = 15;
+        retailerConnectedToDistance[1] = 25;
+        retailerConnectedToDistance[2] = 15;
+        retailerConnectedToDistance[3] = 30;
+        retailerConnectedToDistance[4] = 45;
+        retailerConnectedToDistance[5] = 20;
+        retailerConnectedToDistance[6] = 15;
+        retailerConnectedToDistance[7] = 30;
+        retailerConnectedToDistance[8] = 15;
+        retailerConnectedToDistance[9] = 30;
+
+
         srand((unsigned) time(0));
         for (int locationCreated=0;locationCreated<10;locationCreated++){
             double goodsWeightCreated = (double) (rand() % 501);
@@ -49,9 +76,9 @@ public:
     }
 
     void displayRetailerSites() {
-    std::cout << std::left << std::setw(20) << "Location" << std::setw(20) << "Weight (kg)" << std::setw(20) << "Distance from HQ (km)" << std::endl;
+    std::cout << std::left << std::setw(20) << "Location" << std::setw(20) << "Weight (kg)" << std::setw(30) << "Distance from HQ (km)" << std::setw(30) <<  "Retailer Connected:"<< "Distance (km):" <<std::endl;
     for (int i = 0; i < 10; i++) {
-            std::cout << std::left << std::setw(20) << locations[i] << std::setw(20) << goodsWeight[i] << std::setw(20) << distancesFromHQ[i] << std::endl;
+            std::cout << std::left << std::setw(23) << locations[i] << std::setw(25) << goodsWeight[i] << std::setw(30) << distancesFromHQ[i] << std::setw(27) << retailerConnectedTo[i]<< retailerConnectedToDistance[i]<< std::endl;
         }
     }
 
@@ -60,7 +87,7 @@ public:
         if (file.is_open()) {
             file << "Retailer Locations, Goods Weight, and Distance from HQ List: " << std::endl;
             for (int i = 0; i < 10; i++) {
-                file << "Location: " << locations[i] << ", Weight: " << goodsWeight[i] << "kg, Distance from HQ: " << distancesFromHQ[i] << "km" << std::endl;
+                file << "Location: " << locations[i] << ", Weight: " << goodsWeight[i] << "kg, Distance from HQ: " << distancesFromHQ[i] << "km, Retailer Connected: " << retailerConnectedTo[i] << std::endl;
             }
             file.close();
         } else {
